3ea6e4d636826dd8707df7074a89b94ea63b69fa
Mecit Deniz <=mecit.deniz@extrawatts.co>
2021-05-23 15:34:07 +0300

    Book class

---
+++ LibraryManagementSystem/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LibraryManagementSystem
+{
+    public class Book
+    {
+        private int id;
+        private string name;
+        private string author;
+        private string category;
+
+        public Book(int id, string name, string author, string category)
+        {
+            this.id = id;
+            this.name= name;
+            this.author = author;
+            this.category = category;
+        }
+
+        public int ID { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Author { get => author; set => author = value; }
+        public string Category{ get => category; set => category = value; }
+    }
+}
