f03db9fe46cc14238538dd36637d22b62e0a229e
Mecit Deniz <=mecit.deniz@extrawatts.co>
2021-05-31 01:08:13 +0300

    form validation fixed

--- LibraryManagementSystem/Login.aspx.cs
+++ LibraryManagementSystem/Login.aspx.cs
@@ -12,24 +12,44 @@ namespace LibraryManagementSystem
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+        }
 
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            User user = new User(0, "", "", "");
+
+            user.UserName = textBoxUsername.Text.Trim();
+            user.Password = textBoxPassword.Text.Trim();
+            
+            bool isFormCompleted = validateForm(user.UserName,user.Password);
+            if (!isFormCompleted)
+            {
+                Response.Write("<script>alert('Hay Aksi:( Lütfen formu eksiksiz doldurunuz!');</script>");
+                return;
+            }
+            login(user.UserName,user.Password);
 
         }
 
-        private User user = new User(0,"","","");
+        private bool validateForm(string username,string password)
+        {
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password)) return false;
+            return true;
+        }
 
-        protected void btnLogin_Click(object sender, EventArgs e)
+        private void login(string username,string password)
         {
             try
             {
                 Database db = new Database();
 
-
-                SqlCommand command = new SqlCommand("SELECT * from Users where Username='" +
-                    textBoxUsername.Text.Trim() + "' AND Password='" +
-                    textBoxPassword.Text.Trim() + "' AND isAdmin='" + checkBoxAdmin.Checked + "'", db.Connection());
+                SqlCommand command = new SqlCommand("SELECT * from Users where Username='" + username
+                     + "' AND Password='" +
+                     password + "' AND isAdmin='" + checkBoxAdmin.Checked + "'", db.Connection());
 
                 SqlDataReader sqlDataReader = command.ExecuteReader();
+                User user = new User(0, "", "", "");
+
                 if (sqlDataReader.HasRows)
                 {
                     while (sqlDataReader.Read())
@@ -42,9 +62,9 @@ namespace LibraryManagementSystem
                         if (isAdmin.Equals("True"))
                         {
                             Session.Add("ROLE", "ADMIN");
-                            Session.Add("ID",user.ID);
-                            Session.Add("FULLNAME",user.FullName);
-                            Session.Add("USERNAME",user.UserName);
+                            Session.Add("ID", user.ID);
+                            Session.Add("FULLNAME", user.FullName);
+                            Session.Add("USERNAME", user.UserName);
                             Response.Redirect("UserList.aspx");
                         }
                         else
@@ -59,12 +79,12 @@ namespace LibraryManagementSystem
                 }
                 else
                 {
-                    Response.Write("<script>alert('Invalid Credentials');</script>");
+                    Response.Write("<script>alert('Hay Aksi:( Girdiğiniz bilgiler yanlış!');</script>");
                 }
             }
             catch (Exception ex)
             {
-                Response.Write("<script>alert('"+ex.Message+"');</script>");
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
             }
         }
     }
