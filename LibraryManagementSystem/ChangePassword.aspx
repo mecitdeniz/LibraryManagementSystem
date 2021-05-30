d9c7205722a9a2e9ce061ce2e3bc4f783bbc3747
Mecit Deniz <=mecit.deniz@extrawatts.co>
2021-05-31 00:51:16 +0300

    change password

--- LibraryManagementSystem/ChangePassword.aspx
+++ LibraryManagementSystem/ChangePassword.aspx
@@ -2,5 +2,32 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <h1>Change Password</h1>
+      <div class="row mb-3 align-items-center">
+        <div class="col-md-1">
+            <asp:LinkButton CssClass="row justify-content-center  align-items-center" OnClick="btnBack_Click" runat="server">
+               <i class="fas fa-chevron-left mb-1" style="font-size:24px; color:black;"></i>
+            </asp:LinkButton>
+        </div>
+        <div class="col-md-11">
+            <h3>Şifre Güncelle</h3>
+        </div>
+    </div>
+    <div class="pt-3 for pl-3 pr-3">
+        <form>
+            <div class="form-group">
+                <label>Eski Şifre</label>
+                <asp:TextBox  ID="textBoxPassword" TextMode="Password" CssClass="form-control" runat="server"/>
+            </div>
+            <div class="form-group">
+                <label>Yeni Şifre</label>
+                <asp:TextBox ID="textBoxNewPassword" TextMode="Password" CssClass="form-control" runat="server"/>
+            </div> 
+            <div class="form-group">
+                <label for="textBoxPassword">Yeni Şifre Doğrulama</label>
+                <asp:TextBox ID="textBoxConfirmPassword" TextMode="Password" CssClass="form-control"  runat="server"/>
+            </div>
+          
+            <asp:Button  CssClass="btn btn-success btn btn-block" runat="server" OnClick="btnUpdatePassword_Click" Text="Güncelle" />
+        </form>
+    </div>
 </asp:Content>
--- LibraryManagementSystem/ChangePassword.aspx.cs
+++ LibraryManagementSystem/ChangePassword.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +10,129 @@ namespace LibraryManagementSystem
 {
     public partial class ChangePassword : System.Web.UI.Page
     {
+        private User user = new User(-1, "", "", "");
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            (this.Master as Student).changeButtonCss("btnChangePassword");
+            getUserID();
+        }
 
+
+        protected void btnBack_Click(object sender, EventArgs e)
+        {
+            goBack();
+        }
+
+        protected void btnUpdatePassword_Click(object sender, EventArgs e)
+        {
+            //Get Values
+            string oldPassword = textBoxPassword.Text.Trim().ToString();
+            string newPassword = textBoxNewPassword.Text.Trim().ToString();
+            string confirmPassword = textBoxConfirmPassword.Text.Trim().ToString();
+
+            getUserID();
+
+            //Validate Form
+            bool isPasswordsMatch = validatePassword(newPassword, confirmPassword);
+
+            if (string.IsNullOrEmpty(oldPassword))
+            {
+                Response.Write("<script>alert('Lütfen formu eksiksiz doldurunuz!');</script>");
+                return;
+            }
+
+            if (!isPasswordsMatch)
+            {
+                Response.Write("<script>alert('Girdiğiniz şifreler uyuşmuyor!');</script>");
+                return;
+            }
+
+            //Validate oldPassword
+            bool isPasswordCorrect = validateOldPassword(oldPassword);
+            if (!isPasswordCorrect)
+            {
+                Response.Write("<script>alert('Şifreniz hatalı!');</script>");
+                return;
+            }
+
+            updateUserPassword(oldPassword, newPassword);
+
+        }
+
+
+        private bool validatePassword(string password, string confirmPassword)
+        {
+            if (string.IsNullOrEmpty(password) || string.IsNullOrEmpty(confirmPassword)) return false;
+
+            if (!password.Equals(confirmPassword)) return false;
+            return true;
+        }
+
+
+        public void getUserID()
+        {
+            try
+            {
+                user.ID = int.Parse(Session["ID"].ToString());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                Response.Redirect("Login.aspx");
+            }
+        }
+
+
+        private bool validateOldPassword(string password)
+        {
+            try
+            {
+                Database db = new Database();
+
+                SqlCommand command = new SqlCommand("SELECT * from Users where ID='" +
+                    user.ID + "' AND Password='" + password + "'", db.Connection());
+
+                SqlDataReader sqlDataReader = command.ExecuteReader();
+                if (sqlDataReader.HasRows)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+                return false;
+            }
+
+        }
+
+        private void updateUserPassword(string oldPassword, string newPassword)
+        {
+            try
+            {
+                Database db = new Database();
+                SqlCommand command = new SqlCommand("UPDATE Users SET Password=@Password" +
+                " where ID='" + user.ID + "' and Password= '" + oldPassword + "'", db.Connection());
+
+                command.Parameters.AddWithValue("@Password", newPassword);
+
+                command.ExecuteNonQuery();
+                Response.Write("<script>alert('Şifreniz başarıyla güncellendi! :)');</script>");
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('Hay Aksi! Lütfen eski şifrenizi doğru girdiğinizden emin olunuz!');</script>");
+            }
+        }
+
+        public void goBack()
+        {
+            Response.Redirect("MyBooksList.aspx");
         }
     }
 }
